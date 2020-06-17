0cf36942024a60ad851d0fffa8cb6dcc3d33b290
Andrés Gamboa <andresgamboas@gmail.com>
2020-06-17 16:58:19 -0500

    Add docs for MeasurementList and MeasurementDetail.

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ import os
 from flask import Flask
 from flask_restful import Api
 from flask_mongoengine import MongoEngine
+from flasgger import Swagger
 
 from resources import MeasurementList, MeasurementDetail
-
-
-app = Flask(__name__)
-api = Api(app)
-db = MongoEngine()
 
 API_HOST = os.environ.get('API_HOST', 'localhost')
 DB_HOST = os.environ.get('DB_HOST', 'localhost')
@@ -18,6 +14,8 @@ DB_NAME = os.environ.get('DB_NAME', '')
 DB_USER = os.environ.get('DB_USER', '')
 DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
 
+
+app = Flask(__name__)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': DB_HOST,
@@ -27,7 +25,15 @@ app.config['MONGODB_SETTINGS'] = {
     'password': DB_PASSWORD,
     'authentication_source': 'admin'
 }
-db.init_app(app)
+
+app.config['SWAGGER'] = {
+    'title': 'AHM API',
+    'uiversion': 2
+}
+
+api = Api(app)
+db = MongoEngine(app)
+swagger = Swagger(app)
 
 api.add_resource(MeasurementDetail, '/v1/measurements/<string:id>')
 api.add_resource(MeasurementList, '/v1/measurements')
