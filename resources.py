aa2739412752e9b7f1c54ae3d0fa695d414faed1
Andrés Gamboa <andresgamboas@gmail.com>
2020-06-18 01:09:30 -0500

    Add docs for ReportDetail resource.

--- resources.py
+++ resources.py
@@ -82,7 +82,7 @@ class MeasurementList(Resource):
 
 
 class ReportDetail(Resource):
-
+    @swag_from('docs/report_detail.yml', methods=['GET'])
     def get(self, period):
         try:
             report = Report.objects(period=period).first()
