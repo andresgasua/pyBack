0bf503c5ef71552adc2868c23f529743e3cd7500
Andrés Gamboa <andresgamboas@gmail.com>
2020-06-23 23:03:48 -0500

    Add aggregator script.

--- utils.py
+++ utils.py
@@ -13,5 +13,14 @@ def get_yesterday_date():
     return datetime.utcnow() - timedelta(1)
 
 
-def get_today_date():
-    return datetime.utcnow()
+def get_period(date):
+    if date.day < 16:
+        return get_formatted_date(date.replace(day=15))
+
+    last_day_of_month = get_last_day_of_month(date)
+    return get_formatted_date(date.replace(day=last_day_of_month.day))
+
+
+def get_last_day_of_month(date):
+    next_month = date.replace(day=28) + timedelta(days=4)
+    return next_month - timedelta(days=next_month.day)
